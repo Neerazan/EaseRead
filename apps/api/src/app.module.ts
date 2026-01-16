39b40a5cbe7d40c84693015120e272205046f635
Neerajan Dhakal <nirajandhakal634@gmail.com>
2026-01-16 10:27:41 +0545

    setup configuration

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,9 +1,39 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule, ConfigService, ConfigType } from '@nestjs/config';
+import { validationSchema } from './config/validation.schema';
+import { configs } from './config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import databaseConfig from './config/database.config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: validationSchema,
+      envFilePath: '../../.env',
+      load: configs,
+    }),
+
+    TypeOrmModule.forRootAsync({
+      useFactory: (configService: ConfigService) => {
+        const db =
+          configService.get<ConfigType<typeof databaseConfig>>('database');
+        return {
+          type: 'postgres',
+          host: db?.host,
+          port: db?.port,
+          username: db?.user,
+          password: db?.password,
+          database: db?.database,
+          autoLoadEntities: db?.autoLoadEntities,
+          synchronize: db?.synchronize,
+        };
+      },
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ apps/api/src/config/jwt.config.ts
@@ -0,0 +1,11 @@
+import { registerAs } from '@nestjs/config';
+
+const jwtConfig = registerAs('jwt', () => ({
+  secret: process.env.JWT_SECRET,
+  audience: process.env.JWT_TOKEN_AUDIENCE,
+  issuer: process.env.JWT_TOKEN_ISSUER,
+  access_token_ttl: parseInt(process.env.JWT_ACCESS_TOKEN_TTL!, 10) || 3600,
+  refresh_token_ttl: parseInt(process.env.JWT_REFRESH_TOKEN_TTL!, 10) || 86400,
+}));
+
+export default jwtConfig;
