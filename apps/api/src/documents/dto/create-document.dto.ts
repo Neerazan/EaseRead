3d4d919602a0cdf17c768b57318cbdedcb38f2eb
Neerajan Dhakal <nirajandhakal634@gmail.com>
2026-01-27 13:51:50 +0545

    add support for file upload option in swagger

--- apps/api/src/documents/dto/create-document.dto.ts
+++ apps/api/src/documents/dto/create-document.dto.ts
@@ -1,13 +1,32 @@
-import { IsString, IsOptional, MaxLength } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+import { IsOptional, IsString, MaxLength } from 'class-validator';
 
 export class CreateDocumentDto {
+  @ApiProperty({
+    description: 'The title of the document',
+    required: false,
+    example: 'The Great Gatsby',
+  })
   @IsString()
   @IsOptional()
   @MaxLength(255)
   readonly title?: string;
 
+  @ApiProperty({
+    description: 'The author of the document',
+    required: false,
+    example: 'F. Scott Fitzgerald',
+  })
   @IsString()
   @IsOptional()
   @MaxLength(255)
   readonly author?: string;
+
+  @ApiProperty({
+    type: 'string',
+    format: 'binary',
+    description: 'The file to upload',
+  })
+  @IsOptional()
+  file: any;
 }
